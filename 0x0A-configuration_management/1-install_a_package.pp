45152e40fb4736cb67d3d29a06e3ac0204930930
vicedo19 <victorugochukwuka@gmail.com>
2024-03-22 11:59:15 -0700

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs flask, version 2.1.0
+# Installs puppet-lint, version 2.1.1
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
