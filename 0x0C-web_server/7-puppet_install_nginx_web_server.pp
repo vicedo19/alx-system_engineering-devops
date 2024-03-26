cf3123b80d723805b0ad982ec96d6f9efae0c08c
vicedo19 <victorugochukwuka@gmail.com>
2024-03-26 06:03:00 -0700

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,61 +1,79 @@
 # add stable version of nginx
-exec {'nginx stable repo':
-    command => 'sudo add-apt-repository ppa:nginx/stable',
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    }
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
 
-# update packages
-exec {'apt update':
-    command => 'sudo apt-get update',
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    }
+# update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
 
-# install nginx and ufw firewall
-package {'nginx': ensure    => 'installed'}
-package {'ufw': ensure      => 'installed'}
+# install nginx
+package { 'nginx':
+  ensure     => 'installed',
+}
 
-# Allow nginx: full access
-exec {'grant ufw access to http':
-    command => 'sudo ufw allow "Nginx Full"',
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    }
+# allow HTTP
+exec { 'allow HTTP':
+  command => "ufw allow 'Nginx HTTP'",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  onlyif  => '! dpkg -l nginx | egrep \'îi.*nginx\' > /dev/null 2>&1',
+}
+
+# change folder rights
+exec { 'chmod www folder':
+  command => 'chmod -R 755 /var/www',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
 
 # create index file
-file {'/var/www/html/index.html':
-    ensure  => 'present',
-    content => 'Hello world!\n',
-    }
+file { '/var/www/html/index.html':
+  content => "Hello World!\n",
+}
 
-# create custom 404 error page file
-file {'/var/www/html/custom_404.html':
-    ensure  => 'present',
-    content => "Ceci n'est pas une page\n",
-    }
+# create index file
+file { '/var/www/html/404.html':
+  content => "Ceci n'est pas une page\n",
+}
 
-# configure serve and save in default file in sites-enabled
-file {'/etc/nginx/sites-enabled/default':
-    ensure  => 'present',
-    content => "server {
-                    listen 80 default_server;
-                    listen [::]:80 default_server;
-                    root /var/www/html;
-                    index index.html;
-                    server_name _;
-                    location / {
-                        try_files \$uri \$uri/ =404;
-                    }
-                    error_page 404 /custom_404.html;
-                    location /custom_404.html {
-                        internal;
-                    }
-                    location ~ /redirect_me {
-                        return 301 /www.google.com;
-                    }
-                }",
+# add redirection and error page
+file { 'Nginx default config file':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content =>
+"server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+               root /var/www/html;
+        # Add index.php to the list if you are using PHP
+        index index.html index.htm index.nginx-debian.html;
+        server_name _;
+        location / {
+                # First attempt to serve request as file, then
+                # as directory, then fall back to displaying a 404.
+                try_files \$uri \$uri/ =404;
         }
-
+        error_page 404 /404.html;
+        location  /404.html {
+            internal;
+        }
+        
+        if (\$request_filename ~ redirect_me){
+            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        }
+}
+",
+}
 # restart nginx
-exec {'Restart nginx':
-    command => 'service nginx restart',
-    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-    }
+exec { 'restart service':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
+# start service nginx
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
